cd9de79221d1393e108df66d9fdbf1b3a235fb75
Beau Harrison <beau.harrison@unifysolutions.net>
2020-12-04 15:07:00 +1000

    Day 3, part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -37,6 +37,27 @@ fn parse_pattern(pattern_string: String) -> [[Landscape; PATTERN_WIDTH]; PATTERN
     pattern
 }
 
+fn get_tree_count(
+    pattern: &[[Landscape; PATTERN_WIDTH]; PATTERN_HEIGHT],
+    x_interval: usize,
+    y_interval: usize
+) -> i64 {
+    let mut tree_count = 0;
+
+    let mut x = 0;
+
+    for y in (0..PATTERN_HEIGHT).step_by(y_interval) {
+        if let Landscape::Tree = pattern[y][x] {
+            tree_count += 1;
+        }
+
+        x += x_interval;
+        x = x % PATTERN_WIDTH;
+    }
+
+    tree_count
+}
+
 fn part_1(pattern: &[[Landscape; PATTERN_WIDTH]; PATTERN_HEIGHT]) {
     let mut tree_count = 0;
 
@@ -54,9 +75,34 @@ fn part_1(pattern: &[[Landscape; PATTERN_WIDTH]; PATTERN_HEIGHT]) {
     println!("Part 1 count: {}", tree_count);
 }
 
+fn part_2(pattern: &[[Landscape; PATTERN_WIDTH]; PATTERN_HEIGHT]) {
+    let count_1_1 = get_tree_count(pattern, 1, 1); 
+    let count_3_1 = get_tree_count(pattern, 3, 1); 
+    let count_5_1 = get_tree_count(pattern, 5, 1); 
+    let count_7_1 = get_tree_count(pattern, 7, 1); 
+    let count_1_2 = get_tree_count(pattern, 1, 2);
+    
+    println!("1, 1: {}", count_1_1);
+    println!("3, 1: {}", count_3_1);
+    println!("5, 1: {}", count_5_1);
+    println!("7, 1: {}", count_7_1);
+    println!("1, 2: {}", count_1_2);
+
+    let tree_count: i64 = [ 
+        count_1_1,
+        count_3_1,
+        count_5_1,
+        count_7_1,
+        count_1_2
+    ].iter().fold(1, |acc, x| acc * x);
+
+    println!("Part 1 count: {}", tree_count);
+}
+
 fn main() {
     let input = fs::read_to_string("..\\input\\day3.txt").expect("poop");
     let pattern = parse_pattern(input);  
 
     part_1(&pattern);
+    part_2(&pattern);
 }
