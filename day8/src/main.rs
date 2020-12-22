6f4075ae6b8e6cb4d29fa5b8752fb11a8f6acf1d
Beau Harrison <beau.harrison@unifysolutions.net>
2020-12-22 17:31:41 +1000

    day 8 part 2 done

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,15 +1,11 @@
 use std::fs;
 use std::collections::{HashSet, VecDeque};
 
+#[derive(Clone)]
 enum OpCode {
     ACC(i32),
     JMP(i32),
     NOP(i32)
-}
-
-enum MachineState {
-    Acc: i32
-    Ptr: usize
 }
 
 fn parse_input(input: String) -> Vec<OpCode> {
@@ -66,15 +62,75 @@ fn part_1(instructions: &Vec<OpCode>) {
     println!("part 1 acc: {}", acc);
 }
 
+fn execute_program(instructions: Vec<OpCode>) -> Option<i32> {
+    let mut acc = 0;
+    let mut ptr = 0;
+
+    let mut seen_instructions = HashSet::<usize>::new();
+
+    loop {
+        if seen_instructions.contains(&ptr) {
+            return None;
+        }
+
+        if ptr == instructions.len() {
+            return Some(acc);
+        }
+
+        seen_instructions.insert(ptr);
+        let instruction = instructions.get(ptr);
+        
+        match instruction {
+            Some(OpCode::ACC(v)) => {
+                acc += v;
+                ptr += 1;
+            },
+            Some(OpCode::JMP(v)) => {
+                if *v < 0 {
+                    ptr -= v.abs() as usize;
+                } else {
+                    ptr += *v as usize;
+                }                
+            },
+            Some(OpCode::NOP(_)) => {
+                ptr += 1;
+            },
+            None => panic!("Invalid instruction pointer")
+        }
+    }
+}
+
 
 fn part_2(instructions: &Vec<OpCode>) {
-    let winning = HashSet::<usize>::new();
+    for i in 0..instructions.len() {
+        let swap_instruction_op = instructions.get(i);
 
-    
+        if let Some(op) = swap_instruction_op {
+            if let OpCode::ACC(_) = op {
+                continue;
+            }
 
+            let mut mod_instructions = Vec::<OpCode>::new();
+            mod_instructions.clone_from(instructions);
 
+            match op {
+                OpCode::JMP(jmp_val) => {
+                    mod_instructions[i] = OpCode::NOP(*jmp_val);
+                },
+                OpCode::NOP(nop_val) => {
+                    mod_instructions[i] = OpCode::JMP(*nop_val);
+                },
+                _ => {}
+            }
 
-    println!("part 2 acc: {}", acc);
+            let result = execute_program(mod_instructions);
+
+            if let Some(result_acc) = result {
+                println!("part 2 acc: {}", result_acc);
+                break;
+            }
+        }    
+    }    
 }
 
 fn main() {
